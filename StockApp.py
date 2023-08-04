5610442a64f46f1bcac5363338fc129e743da236
Oladimeji Kazeem <97573171+oladimeji-kazeem@users.noreply.github.com>
2023-08-04 18:03:30 +0100

    Updated the code to display individual stocks
    Updated the code to display individual stocks with the dataframe and plots.

--- StockApp.py
+++ StockApp.py
@@ -1,35 +1,28 @@
 import yfinance as yf
 import pandas as pd
 import plotly.graph_objs as go
-from sklearn.linear_model import LinearRegression
 import streamlit as st
 
-def get_stock_data(stock_symbol):
-    company = yf.Ticker(stock_symbol)
-    return company
+def get_stock_data(stock_symbols, start_date, end_date):
+    companies = [yf.Ticker(symbol) for symbol in stock_symbols]
+    stock_data = [yf.download(symbol, start=start_date, end=end_date) for symbol in stock_symbols]
+    summaries = [company.info['longBusinessSummary'] for company in companies]
+    return companies, stock_data, summaries
 
-apl = get_stock_data("AAPL")
-mst = get_stock_data("MSFT")
-tsl = get_stock_data("TSLA")
-gog = get_stock_data("GOGL")
+# Streamlit sidebar widgets to allow user input
+selected_stocks = st.sidebar.multiselect("Select Stock Symbols", ["AAPL", "MSFT", "TSLA", "GOOG"], default=["AAPL"])
+start_date = st.sidebar.date_input("Start Date", value=pd.to_datetime("2015-01-01"))
+end_date = st.sidebar.date_input("End Date", value=pd.to_datetime("2022-12-31"))
 
-apple = yf.download("AAPL", start="2015-01-01", end="2022-12-31")
-microsoft = yf.download("MSFT", start="2015-01-01", end="2022-12-31")
-tesla = yf.download("TSLA", start="2015-01-01", end="2022-12-31")
-google = yf.download("GOGL", start="2015-01-01", end="2022-12-31")
+companies, stock_data, summaries = get_stock_data(selected_stocks, start_date, end_date)
 
-#Fetching the historical data by valid periods
-dt1 = apl.history(period="5y")
-dt2 = mst.history(period="5y")
-dt3 = tsl.history(period="5y")
-dt4 = gog.history(period="5y")
+# Displaying information and charts using Streamlit
+for i, company in enumerate(companies):
+    st.write(f"### {selected_stocks[i]}")
+    st.markdown(f"**{company.info['longName']}**")
+    st.write(summaries[i][:200] + "...")
+    st.write("[Read more](#)")  # Placeholder link to be replaced later with a real link
+    st.write(stock_data[i])
 
-#Markdown
-st.write(""" ### Apple""")
-#Detailed summary about apple
-st.write(apl.info['longBusinessSummary'])
-#DataFrame
-st.write(apple)
-#Charting
-st.line_chart(dt1.values)
-
+    # Plotting the line chart
+    st.line_chart(stock_data[i]["Close"])
